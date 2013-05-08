75b8c663c49e797cf5f80e6ec71176e1edde3954
Ahf <mail_ahf@yahoo.co.jp>
2013-05-08 14:29:39 +0900

    サンプルソースの初期コミット

---
+++ Cod2013jp_AppHarbor/Controllers/HomeController.vb
@@ -0,0 +1,21 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Cod2013 北海道会場 #1"
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Your app description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+End Class
---
+++ Cod2013jp_AppHarbor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Cod2013jp_AppHarbor.MvcApplication" Language="VB" %>
